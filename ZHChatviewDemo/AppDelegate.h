73380832730e7ac72efb7c666f2f7c5e862d3a54
ZhaoHan <zhaohan@zhaohandemacbook-pro.local>
2016-04-20 21:08:06 +0800

    Initial Commit

---
+++ ZHChatviewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHChatviewDemo
+//
+//  Created by ZhaoHan on 16/4/20.
+//  Copyright © 2016年 ZhaoHan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZHChatviewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZHChatviewDemo
+//
+//  Created by ZhaoHan on 16/4/20.
+//  Copyright © 2016年 ZhaoHan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
